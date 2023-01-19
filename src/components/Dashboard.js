5269d1c01e0386651ef6ec17bce53fc8ef87ef3a
Kennie <Kenneth Chan>
2023-01-19 16:25:04 -0500

    add mock data and display interactive panels

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,65 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
+import Panel from "./Panel";
 
 import classnames from "classnames";
 
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
 
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused" : this.state.focused
+    });
+    
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
+      />
+    ));
+    
+    return <main className={dashboardClasses}> { panels } </main>;
   }
 }
 
